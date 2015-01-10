bd20a75d79a6438aaf7384735e23eeb4ad040b8d
rafatpc <rafa_tpc@live.co.uk>
2015-01-10 16:03:29 +0200

    Publish new ads

--- js/app.js
+++ js/app.js
@@ -7,18 +7,23 @@ app.constant('pageSize', 2);
 
 app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
-            templateUrl: 'templates/home.html',
+            templateUrl: '/templates/home.html',
             controller: 'HomeController'
         });
 
         $routeProvider.when('/login', {
-            templateUrl: 'templates/login.html',
+            templateUrl: '/templates/login.html',
             controller: 'LoginController'
         });
 
         $routeProvider.when('/register', {
-            templateUrl: 'templates/register.html',
+            templateUrl: '/templates/register.html',
             controller: 'RegisterController'
+        });
+
+        $routeProvider.when('/user/ads/publish', {
+            templateUrl: '/templates/user/newad.html',
+            controller: 'BlankController'
         });
 
         $routeProvider.otherwise({
